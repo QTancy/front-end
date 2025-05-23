f6d255b37c1d94f1866f841adc8ae9f9e9dec64c
rachelle <fc010d5x0499@student.devacademy.id>
2025-05-23 23:02:14 +0700

    feat: :sparkles: membuat qrep
    masih raw, blm pake icons, style blm bener

---
+++ src/app/(homepage)/qrep/page.js
@@ -0,0 +1,11 @@
+'use client';
+
+import ReceiptAnalyse from '@/components/ui/qrep/ReceiptAnalyse';
+
+export default function QrepPage() {
+  return (
+    <main className="body-color-background min-h-screen p-6">
+      <ReceiptAnalyse />
+    </main>
+  );
+}
