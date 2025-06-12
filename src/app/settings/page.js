33bba4f928ff7c43db63bb8ba8f82297aea95f11
ulayya <ulayyaathifal@gmail.com>
2025-06-12 15:26:05 +0700

    feat: :art: add settings

---
+++ src/app/settings/page.js
@@ -0,0 +1,16 @@
+'use client';
+
+import HeaderApps from '@/components/ui/headerapps/HeaderApps';
+import RegistrationForm from '@/components/ui/settings/SettingForm';
+
+export default function SettingsPage() {
+  return (
+    <>
+      <HeaderApps />
+      <main className="body-color-background min-h-screen p-6">
+        <RegistrationForm />
+      </main>
+    </>
+  );
+}
+
